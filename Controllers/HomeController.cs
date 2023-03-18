d9eb9d8566f0bb881baf84540b9ae4f364208236
KhanKhad <xxanov2017@mail.ru>
2023-03-18 13:26:23 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StartupsBack.Database;
+using StartupsBack.Models;
+using System.Diagnostics;
+
+namespace StartupsBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly MainDb _dbContext;
+        public HomeController(ILogger<HomeController> logger, MainDb dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public async Task Index()
+        {
+            Response.ContentType = "text/html;charset=utf-8";
+            await Response.WriteAsync(_dbContext.UserDB.ToList().Count.ToString());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Database/MainDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StartupsBack.Database
+{
+    public class MainDb : DbContext
+    {
+        public DbSet<Datacell> UserDB { get; set; } = null!;
+
+        public MainDb(DbContextOptions<MainDb> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Datacell>();
+        }
+    }
+    public class Datacell
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string OpenKeyXML { get; set; } = string.Empty;
+        public string OpenKeyPem { get; set; } = string.Empty;
+        public int keyValid { get; set; }
+        public string GettedMessages { get; set; } = string.Empty;
+        public string SendedMessages { get; set; } = string.Empty;
+    }
+}
